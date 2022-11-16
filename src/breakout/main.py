42b1abd07c9b9df987964a115c2436df929065ad
tristntran <tristntran@gmail.com>
2022-11-16 19:25:21 +0000

    adding in some basics

--- src/breakout/main.py
+++ src/breakout/main.py
@@ -1,21 +1,27 @@
-"""
-"""
-from trainer import init_model
+import time
+
+import gym
+import numpy as np
+import os
+
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.env_util import make_vec_env
 
-def main():
-    # multiprocess environment
-    n_steps = 10000000
-    save_every_n = 500000
-    
-    model = init_model(PPO,'MlpPolicy','CartPole-v1',n_steps)
-    count = 0
-    while count <= n_steps:
-        model.learn(total_timesteps=save_every_n)
-        file_name = 'ppo2_cartpole'
-        model.save(f"models/{file_name}_steps{count}")
-        count += save_every_n
+from typing import Callable
 
-if __name__ == "__main__":
-    print("FOO")
-    main()
+if __name__=='__main__':
+    env_id = "Breakout-v4"
+    num_cpu = 6  # Number of processes to use
+    # Create the vectorized environment
+    # By default, we use a DummyVecEnv as it is usually faster (cf doc)
+    vec_env = make_vec_env(env_id, n_envs=num_cpu)
+
+    model = PPO('MlpPolicy', vec_env, verbose=1)
+    os.makedirs("model", exist_ok=True)
+    n_steps_arr = [10, 100, 1000,10000, 100000]
+    for n_steps in n_steps_arr:
+        model.learn(n_steps)
+        model.save(f"model/breakout{n_steps}")
